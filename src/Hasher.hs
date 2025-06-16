80e2e13a91a18a93588b3025109873f0c06d6117
Vlad Stashevskyi <98583506+voadosik@users.noreply.github.com>
2025-06-16 02:02:03 +0200

    Update Hasher.hs

--- src/Hasher.hs
+++ src/Hasher.hs
@@ -3,5 +3,8 @@ module Hasher (rollingHash) where
 import qualified Data.ByteString as BS
 import Data.Bits (xor, shiftL)
 
+-- Compute a rolling hash of a file's contents
+-- XOR current byte with accumulated hash, shifts left
+-- Not strong, but fast
 rollingHash :: BS.ByteString -> Integer
 rollingHash = BS.foldl' (\h b -> (h `xor` fromIntegral b) `shiftL` 1) 0
